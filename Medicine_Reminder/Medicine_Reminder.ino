0e002990172176341cc153d957f8902a89c78699
Cossette Alexis Gabuya <gabuyalexis@gmail.com>
2019-10-13 01:24:58 +0800

    Update Medicine_Reminder.ino

--- Medicine_Reminder/Medicine_Reminder.ino
+++ Medicine_Reminder/Medicine_Reminder.ino
@@ -1,23 +1,20 @@
-/* ----- C Program for Arduino based Alarm Clock ---- */
 
 #include <Wire.h>
 #include<EEPROM.h>
 #include <RTClib.h>
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(3, 2, 4, 5, 6, 7); 
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2); 
 RTC_DS1307 RTC;
 int temp,inc,hours1,minut,add=11;
-
 int next=10;    
-int INC=11;
-int set_mad=12;
+int INC=9;
+int set_mad=8;
 
 #define buzzer 13
 
 int HOUR,MINUT,SECOND;
 
- 
 void setup()
 {
  Wire.begin();
@@ -27,39 +24,56 @@ void setup()
  pinMode(next, INPUT);
  pinMode(set_mad, INPUT);
  pinMode(buzzer, OUTPUT);
- digitalWrite(next, HIGH);
- digitalWrite(set_mad, HIGH);
- digitalWrite(INC, HIGH);
- 
+
    lcd.setCursor(0,0);
-   lcd.print("Real Time Clock");
+   lcd.print("Medicin reminder");
    lcd.setCursor(0,1);
-   lcd.print("Circuit Digest ");
+   lcd.print(" Using Arduino  ");
     delay(2000);
-    
+    lcd.setCursor(0,0);
+   lcd.print("By Saddam khan  ");
+   lcd.setCursor(0,1);
+   lcd.print("Engineers Garage");
+    delay(2000);
+
  if(!RTC.isrunning())
  {
  RTC.adjust(DateTime(__DATE__,__TIME__));
  }
 }
- 
+
 void loop()
 {
    int temp=0,val=1,temp4;
    DateTime now = RTC.now();
-   if(digitalRead(set_mad) == 0)      //set Alarm time
+   if(digitalRead(set_mad) == 0)      //set medicine time
    { 
      lcd.setCursor(0,0);
-     lcd.print("  Set Alarm  ");
+   lcd.print("  Set Medicine  ");
+   lcd.setCursor(0,1);
+   lcd.print("  Reminder time ");
     delay(2000);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Enter Time 1");
     defualt();
-    time();
+    time(1);
     delay(1000);
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("  Alarm time ");
-    lcd.setCursor(0,1);
-    lcd.print(" has been set  ");
+    lcd.print("Enter Time 2");
+    defualt();
+    delay(1000);
+    time(2);
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Enter Time 3");
+    defualt();    
+time(3);
+     lcd.setCursor(0,0);
+   lcd.print("Medicin reminder");
+   lcd.setCursor(0,1);
+   lcd.print("  time has set  ");
     delay(2000);
  }
  lcd.clear();
@@ -94,7 +108,7 @@ void defualt()
 
 /*Function to set alarm time and feed time into Internal eeprom*/
 
-void time()                             
+void time(int x)  
 {
   int temp=1,minuts=0,hours=0,seconds=0;
     while(temp==1)
@@ -107,11 +121,11 @@ void time()
        HOUR=0;
       }
       while(digitalRead(INC)==0);
-     }
-     lcd.clear();
+     }     
+lcd.clear();
       lcd.setCursor(0,0);
-    lcd.print("Set Alarm Time ");
-   //lcd.print(x); 
+    lcd.print("Enter Time ");
+   lcd.print(x); 
     lcd.setCursor(0,1);
     lcd.print(HOUR);
     lcd.print(":");
@@ -126,18 +140,20 @@ void time()
      temp=2;
      while(digitalRead(next)==0);
     }
-    }
-    
+    } 
     while(temp==2)
-    {
-     if(digitalRead(INC)==0)
+    {     
+if(digitalRead(INC)==0)
      {
       MINUT++;
       if(MINUT==60)
       {MINUT=0;}
       while(digitalRead(INC)==0);
      }
-     // lcd.clear();
+      lcd.clear();
+      lcd.setCursor(0,0);
+    lcd.print("Enter Time ");
+   lcd.print(x); 
     lcd.setCursor(0,1);
     lcd.print(HOUR);
     lcd.print(":");
@@ -156,6 +172,7 @@ void time()
     delay(1000);
 }
 
+
 /* Function to chack medication time */
 
 void match()
@@ -163,24 +180,56 @@ void match()
   int tem[17];
   for(int i=11;i<17;i++)
   {
-    tem[i]=EEPROM.read(i);
-  }
+    tem[i]=EEPROM.read(i); 
+ }
   if(HOUR == tem[11] && MINUT == tem[12]) 
   {
    beep();
    beep();
    beep();
    beep();
-   lcd.clear();
-   lcd.print("Wake Up........");
+   lcd.setCursor(0,0);
+   lcd.print("  Take Group One  ");
    lcd.setCursor(0,1);
-   lcd.print("Wake Up.......");
+   lcd.print("     Medicine     ");
    beep();
    beep();
    beep();
    beep();
   }
 
+   if(HOUR == tem[13] && MINUT == tem[14])
+   {
+      beep();
+   beep();
+   beep();
+   beep();
+    lcd.setCursor(0,0);
+   lcd.print("  Take Group Two  ");
+   lcd.setCursor(0,1);
+   lcd.print("     Medicine     ");
+    beep();
+   beep();
+   beep();
+   beep();
+   }
+
+
+   if(HOUR == tem[15] && MINUT == tem[16] )
+   {  
+     beep();
+   beep();
+   beep();
+   beep();
+    lcd.setCursor(0,0);
+   lcd.print("Take Group Three ");
+   lcd.setCursor(0,1);
+   lcd.print("     Medicine    ");
+   beep();
+   beep();
+   beep();
+   beep();
+   }
 }
 
 /* function to buzzer indication */
